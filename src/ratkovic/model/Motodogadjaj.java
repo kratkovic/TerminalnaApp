3594fd7c3b0d0158cfcf8ab06696200db0009768
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-02-06 22:00:48 +0100

    nastavak aplikacije

---
+++ src/ratkovic/model/Motodogadjaj.java
@@ -0,0 +1,62 @@
+package ratkovic.model;
+
+import java.util.Date;
+
+public class Motodogadjaj extends Entitet {
+	
+	private String naziv;
+	private String mjestoOdrzavanja;
+	private Date datumPocetka;
+	private String odgovorniClan;
+	private Motoklub motoklub;
+	
+	public Motodogadjaj() {
+		super();
+	}
+	public Motodogadjaj(int sifra, String naziv, String mjestoOdrzavanja, Date datumPocetka, String odgovorniClan,
+			Motoklub motoklub) {
+		super(sifra);
+		this.naziv = naziv;
+		this.mjestoOdrzavanja = mjestoOdrzavanja;
+		this.datumPocetka = datumPocetka;
+		this.odgovorniClan = odgovorniClan;
+		this.motoklub = motoklub;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getMjestoOdrzavanja() {
+		return mjestoOdrzavanja;
+	}
+	public void setMjestoOdrzavanja(String mjestoOdrzavanja) {
+		this.mjestoOdrzavanja = mjestoOdrzavanja;
+	}
+	public Date getDatumPocetka() {
+		return datumPocetka;
+	}
+	public void setDatumPocetka(Date datumPocetka) {
+		this.datumPocetka = datumPocetka;
+	}
+	public String getOdgovorniClan() {
+		return odgovorniClan;
+	}
+	public void setOdgovorniClan(String odgovorniClan) {
+		this.odgovorniClan = odgovorniClan;
+	}
+	public Motoklub getMotoklub() {
+		return motoklub;
+	}
+	public void setMotoklub(Motoklub motoklub) {
+		this.motoklub = motoklub;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/ratkovic/model/Smjestaj.java
@@ -0,0 +1,52 @@
+package ratkovic.model;
+
+import java.math.BigDecimal;
+
+public class Smjestaj extends Entitet{
+	
+	private String naziv;
+	private String vrsta;
+	private BigDecimal cijena;
+	private Motodogadjaj motodogadjaj;
+	
+		
+	public Smjestaj() {
+		super();
+	}
+	public Smjestaj(int sifra, String naziv, String vrsta, BigDecimal cijena, Motodogadjaj motodogadjaj) {
+		super(sifra);
+		this.naziv = naziv;
+		this.vrsta = vrsta;
+		this.cijena = cijena;
+		this.motodogadjaj = motodogadjaj;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getVrsta() {
+		return vrsta;
+	}
+	public void setVrsta(String vrsta) {
+		this.vrsta = vrsta;
+	}
+	public BigDecimal getCijena() {
+		return cijena;
+	}
+	public void setCijena(BigDecimal cijena) {
+		this.cijena = cijena;
+	}
+	public Motodogadjaj getMotodogadjaj() {
+		return motodogadjaj;
+	}
+	public void setMotodogadjaj(Motodogadjaj motodogadjaj) {
+		this.motodogadjaj = motodogadjaj;
+	}
+	
+	
+	
+}
+	
+	
